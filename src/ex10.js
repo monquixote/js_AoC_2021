05414e87ddbf111a7c67572dfa543a35576139e5
Nick Long <nick.long@gmail.com>
2021-12-11 15:14:54 +0000

    Ex10

--- src/ex10.js
+++ src/ex10.js
@@ -25,20 +25,16 @@ const scores = new Map(
 );
 
 function parser([head, ...tail]) {
-    if(!head) {
-        console.log('No head')
-        return 0;
-    }
     let remainder = tail;
-    while(remainder.length > 0) {
-        if([...matchedPairs.values()].includes(remainder[0])) {
-            if(remainder[0] === matchedPairs.get(head)) {
+    while (remainder.length > 0) {
+        if ([...matchedPairs.values()].includes(remainder[0])) {
+            if (remainder[0] === matchedPairs.get(head)) {
                 return remainder.slice(1);
             }
             return scores.get(remainder[0]);
         }
         const result = parser(remainder);
-        if(Number.isInteger(result)) {
+        if (Number.isInteger(result)) {
             return result;
         }
         remainder = result;
@@ -48,7 +44,57 @@ function parser([head, ...tail]) {
 
 const results = inputTokens.map(parser);
 
-const [corrupt, incomplete] = _.partition(results, Number.isInteger);
+const [corrupt, incomplete] = _.partition(results, (x) => Number.isInteger(x) && x > 0);
 
-const ans = corrupt.reduce((x,y) => x+y)
+const ans = corrupt.reduce((x, y) => x + y)
 console.log(ans);
+
+const inc = inputTokens.filter((x, i) => !Number.isInteger(results[i]) || results[i] === 0)
+
+//console.log(inc)
+// console.log(incomplete)
+
+
+function autoComplete(listing) {
+    let end = []
+    function parser2([head, ...tail]) {
+        let remainder = tail;
+        while (remainder.length > 0) {
+            if ([...matchedPairs.values()].includes(remainder[0])) {
+                if (remainder[0] === matchedPairs.get(head)) {
+                    // console.log('return')
+                    return remainder.slice(1);
+                }
+                e = new Error()
+                e.score = scores.get(remainder[0]);
+                throw e;
+            }
+            const result = parser2(remainder);
+            remainder = result;
+        }
+        end.push(matchedPairs.get(head))
+        return [];
+    }
+    while (listing.length > 0) {
+        listing = parser2(listing);
+    }
+    return end;
+}
+
+// console.log(inc.map(x => x.join('')))
+const completions = inc.map(autoComplete);
+
+const completionScores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+const completionTotals = completions
+    .map(x => x
+        .reduce((total, c) => total * 5 + completionScores[c], 0));
+
+
+console.log(completionTotals.sort((x, y) => x - y)[Math.floor(completionTotals.length / 2)])
+
