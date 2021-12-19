4ba1e00e299aa1ed8b9076742290de00e212ce00
Nick Long <nick.long@gmail.com>
2021-12-19 18:38:21 +0000

    Ex 16

--- src/ex16.js
+++ src/ex16.js
@@ -15,16 +15,6 @@ function processHex(input) {
         .flat();
 }
 
-/*
-function gen.take( no) {
-    const results = [];
-    for (let i = 0; i < no; i++) {
-        results.push(gen.next().value);
-    }
-    return results;
-}
-*/
-
 class ReaderGen {
     constructor(bitStream) {
         this.bitStream = bitStream;
@@ -42,19 +32,10 @@ class ReaderGen {
         }
         return result;
     }
-
     done() {
         return this.index >= this.bitStream.length
     }
-    align() {
-        if (this.index % 4) {
-            const remainder = 4 - (this.index % 4)
-            console.log('remainder', remainder)
-            this.index += remainder;
-        }
-    }
 }
-
 
 function arr2Bin(x) {
     return parseInt(x.join(''), 2)
@@ -66,7 +47,7 @@ function readHeader(gen) {
     const version = arr2Bin(gen.take(3))
     const type = arr2Bin(gen.take(3))
     versionTotal += version;
-    return [version, type]
+    return type
 }
 
 function readLiteralPacket(gen) {
@@ -82,9 +63,6 @@ function readLiteralPacket(gen) {
 
     const val = arr2Bin(valArr);
 
-    // gen.align();
-
-    console.log('Reading val', val)
     return Number(val);
 }
 
@@ -95,19 +73,14 @@ const TYPE_LITERAL = 4;
 
 function readOpLen(gen) {
 
-
     const [lengthType] = gen.take(1);
-
-    console.log('Reading op', lengthType)
 
     if (lengthType === 0) {
         const bits = gen.take(BIT_LEN_BITS);
         const bitLength = arr2Bin(bits)
-        console.log('bit len', bitLength)
         return { bitLength, packetLength: null }
     } else if (lengthType === 1) {
         const packetLength = arr2Bin(gen.take(PACKET_LEN_BITS))
-        console.log('packet len', packetLength)
         return { bitLength: null, packetLength }
     } else {
         throw Error(lengthType)
@@ -116,9 +89,7 @@ function readOpLen(gen) {
 }
 
 function ex2Proc(gen) {
-    const [version, type] = readHeader(gen);
-    console.log('v', version, 't', type)
-
+    const type = readHeader(gen);
 
     if (type === TYPE_LITERAL) {
         return readLiteralPacket(gen)
@@ -162,4 +133,3 @@ const binArr = inputBin
 
 const readerGen = new ReaderGen(binArr);
 console.log(ex2Proc(readerGen), versionTotal)
-
