38cd581d935df032340ef1c042096a28f06d5ea2
Nick Long <nick.long@gmail.com>
2021-12-19 18:34:46 +0000

    Ex 16

--- src/ex16.js
+++ src/ex16.js
@@ -60,9 +60,12 @@ function arr2Bin(x) {
     return parseInt(x.join(''), 2)
 }
 
+let versionTotal = 0;
+
 function readHeader(gen) {
     const version = arr2Bin(gen.take(3))
     const type = arr2Bin(gen.take(3))
+    versionTotal += version;
     return [version, type]
 }
 
@@ -82,7 +85,7 @@ function readLiteralPacket(gen) {
     // gen.align();
 
     console.log('Reading val', val)
-    return val;
+    return Number(val);
 }
 
 const BIT_LEN_BITS = 15;
@@ -90,7 +93,7 @@ const PACKET_LEN_BITS = 11;
 
 const TYPE_LITERAL = 4;
 
-function readOperator(gen) {
+function readOpLen(gen) {
 
 
     const [lengthType] = gen.take(1);
@@ -112,88 +115,51 @@ function readOperator(gen) {
 
 }
 
-function ex1Processor(gen) {
+function ex2Proc(gen) {
+    const [version, type] = readHeader(gen);
+    console.log('v', version, 't', type)
 
-    let total = 0;
 
-    let packetCount = 0;
-    let bitLength = null;
-    let packetLength = null;
-    let limitSet = false;
-
-    try {
-        while (!gen.done()) {
-            /*
-            if (packetLength && packetCount >= packetLength) {
-                console.log('Packet Count ', packetCount)
-                break;
-            }
-            if (bitLength && gen.index >= bitLength) {
-                console.log('Bit Length ', bitLength, gen.index)
-                break;
-            }
-            */
-            const [version, type] = readHeader(gen);
-            if (!Number.isInteger(version)) {
-                console.log('Done')
-                break;
-            }
-            console.log('v', version, 't', type)
-            total += version;
-
-            if (type === TYPE_LITERAL) {
-                readLiteralPacket(gen)
-
-            } else {
-                const lens = readOperator(gen)
-                if (!limitSet) {
-                    console.log('setting limit', lens)
-                    packetLength = lens.packetLength;
-                    if (bitLength) {
-                        bitLength = lens.bitLength + gen.index;
-                    }
-                    limitSet = true;
-                }
-            }
-
-            packetCount++;
-
-        }
-    } catch(e) {
-        console.log('oops hit the end!')
+    if (type === TYPE_LITERAL) {
+        return readLiteralPacket(gen)
     }
 
-    return total;
+    const { bitLength, packetLength } = readOpLen(gen);
+
+    const startBits = gen.index;
+    let packets = 0;
+    const operands = []
+
+    while (!timeToStop(bitLength, packetLength, gen.index - startBits, packets)) {
+        operands.push(ex2Proc(gen))
+        packets++
+    }
+
+    return operators[String(type)](operands)
+}
+
+function timeToStop(bitLength, packetLength, bits, packets) {
+    if (bitLength) {
+        return bits >= bitLength
+    }
+    if (packetLength) {
+        return packets >= packetLength
+    }
+    throw Error('Should be one or the other!')
+}
+
+const operators = {
+    '0': (args) => args.reduce((a, b) => a + b),
+    '1': (args) => args.reduce((a, b) => a * b),
+    '2': (args) => Math.min(...args),
+    '3': (args) => Math.max(...args),
+    '5': ([x,y]) => x > y ? 1 : 0,
+    '6': ([x,y]) => x < y ? 1 : 0,
+    '7': ([x,y]) => x === y ? 1 : 0
 }
 
 const binArr = inputBin
 
 const readerGen = new ReaderGen(binArr);
-console.log(ex1Processor(readerGen))
-
-
-
-// EX1 DEBUG
-// const binArr = processHex("8A004A801A8002F478")
-
-//Ex 1
-// const binArr = "00111000000000000110111101000101001010010001001000000000".split('').map(Number)
-// console.log(processHex("38006F45291200").join(''));
-
-
-// const binArr = "110100101111111000101000".split('').map(Number)
-
-// const readerGen = new ReaderGen(binArr);
-
-// Example 2
-// const binArr = "11101110000000001101010000001100100000100011000001100000".split('').map(Number)
-
-
-// 8A004A801A8002F478
-// 4 1 5 6  = 16
-// const binArr = processHex("8A004A801A8002F478")
-
-// const binArr = processHex("620080001611562C8802118E34")
-
-// const binArr = processHex("A0016C880162017C3686B18A3D4780")
+console.log(ex2Proc(readerGen), versionTotal)
 
